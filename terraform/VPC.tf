ddff833d43549051480127f232f3713f560bb181
fabian leiva <fleiva@gbm.net>
2025-04-25 14:59:52 -0600

    primer commit terraform

---
+++ terraform/VPC.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "mi_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "VPC-Terraform-Test"
+  }
+}
