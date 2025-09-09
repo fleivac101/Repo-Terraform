abe3fa2b745ace90d77acfb4699d3f32a418cb58
fleivac101 <51421830+fleivac101@users.noreply.github.com>
2025-09-09 10:55:20 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,64 @@
+## Plantilla específica para la creación de una máquina virtual con 2 discos mediante clonación ##
+## de un Template existente en VMWare ##
+
+data "vsphere_datacenter" "datacenter" {
+  name = var.datacenter_name
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vm_name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = var.cpu_number
+  memory           = var.memory_amount
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+  scsi_controller_count = data.vsphere_virtual_machine.template.scsi_controller_scan_count
+  firmware         = data.vsphere_virtual_machine.template.firmware
+  wait_for_guest_net_timeout = -1
+  memory_hot_add_enabled = true
+  num_cores_per_socket = var.cpu_number
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = var.disk_size_0
+  }
+
+  clone {
+      template_uuid = data.vsphere_virtual_machine.template.id
+      customize {
+        linux_options {
+          host_name = var.host_name
+          domain    = var.host_domain
+        }
+        network_interface {
+          ipv4_address = var.ip_address
+          ipv4_netmask = 24
+        }
+      }
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,84 @@
+## Datos de Login ##
+
+variable "vsphere_user" {
+  description = "Usuario de Conexión"
+  type = string
+}
+
+variable "vsphere_password" {
+  description = "Contraseña del Usuario"
+  type = string
+}
+
+
+variable "vsphere_server" {
+  description = "IP del VCenter"
+  type = string
+}
+
+## Datos del vSphere ##
+
+# Son variables de qué recursos del vCenter utilizará la máquina virtual #
+variable "datacenter_name" {
+  description = "Nombre del Data Center"
+  type = string
+}
+
+variable "datastore_name" {
+  description = "Nombre del Data Store"
+  type = string
+}
+
+variable "cluster_name" {
+  description = "Nombre del Cluster"
+  type = string
+}
+
+variable "network_name" {
+  description = "Nombre de la Network"
+  type = string
+}
+## Datos del vSphere ##
+
+## Datos de la customización de la Máquina Virtual ##
+## Son datos de las caracteristicas de Hardware y S.O. de la máquina ##
+variable "disk_size_0" {
+  description = "Tamaño del Disco 0"
+  type = number
+}
+
+variable "template_name" {
+  description = "Nombre del Template de la Maquina Virtual"
+  type = string
+}
+
+variable "vm_name" {
+  description = "Nombre de la Maquina Virtual a nivel de VMWare"
+  type = string
+}
+
+variable "cpu_number" {
+  description = "Numero de CPUs"
+  type = number
+}
+
+variable "memory_amount" {
+  description = "Cantidad de Memoria de la Maquina"
+  type = number
+}
+
+variable "host_name" {
+  description = "Nombre de la Máquina a Nivel de S.O."
+  type = string
+}
+
+variable "host_domain" {
+  description = "Dominio de la Máquina a Nivel de S.O."
+  type = string
+}
+
+variable "ip_address" {
+  description = "Direccion IP de la Maquina"
+  type = string
+}
+## Datos de la customización de la Máquina Virtual ##
