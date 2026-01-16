59d287b9d4d361a065452d2027cb27a61927a0e4
fleivac101 <51421830+fleivac101@users.noreply.github.com>
2026-01-16 13:43:25 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/vsphere"
       version = "~> 2.7"
     }
-
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
   }
 }
 
@@ -17,8 +12,4 @@ provider "vsphere" {
   password             = var.vsphere_password
   vsphere_server       = var.vsphere_server
   allow_unverified_ssl = true
-}
-
-provider "azurerm" {
-  features {}
 }
